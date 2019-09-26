6714dda3d8afcbffcf9706586e6d648107d4a9d4
farhan3211 <55830838+farhan3211@users.noreply.github.com>
2019-09-26 16:22:13 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118054/latihan9/teks/Pboif210118054Latihan9Teks.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118054.latihan9.teks;
+
+/**
+ *
+ * @author ASUS
+ * nama  :Farhan Milardi
+ * kelas :IF-2 
+ * nim   :10118054
+ */
+public class Pboif210118054Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     * har c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char: " + tab + "berisi " + c);
+
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
